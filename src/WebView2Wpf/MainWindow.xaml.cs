6c3826cf582bb031a07f5c22914916b5214197f8
Rendijs Smukulis <rendijs.smukulis@gmail.com>
2023-10-24 17:18:30 +0300

    Created barebones 'browser'

---
+++ src/WebView2Wpf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.Windows;
+
+namespace WebView2Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.webview.CoreWebView2InitializationCompleted += WebviewOnCoreWebView2InitializationCompleted;
+
+            var options = new CoreWebView2EnvironmentOptions();
+            var e = CoreWebView2Environment.CreateAsync(options: options).Result;
+            this.webview.EnsureCoreWebView2Async(e);
+        }
+
+        private void WebviewOnCoreWebView2InitializationCompleted(object? sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            this.webview.CoreWebView2.Navigate("https://news.ycombinator.com");
+            this.webview.CoreWebView2.NavigationCompleted += CoreWebView2OnNavigationCompleted;
+        }
+
+        private void CoreWebView2OnNavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            this.urlToNavTo.Text = this.webview.Source.OriginalString;
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.webview.CoreWebView2.Navigate(this.urlToNavTo.Text);
+        }
+    }
+}
